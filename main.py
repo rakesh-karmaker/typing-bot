fecd5d875f295aec0de658fb45262e4bd7344ed9
Rakesh Karmaker <rakeshgaming12345@gmail.com>
2024-12-04 00:35:03 +0600

    add get_text function

--- main.py
+++ main.py
@@ -6,24 +6,23 @@ import pyautogui
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://humanbenchmark.com/tests/typing"
 
 
-def get_text(driver):
+def get_text(driver, url):
     driver.get(url)
     source_code = driver.page_source
     soup = BeautifulSoup(source_code, "html.parser")
     spans = soup.find_all("span", class_="incomplete")
     return "".join([span.text for span in spans])
 
+def main():
+    url = "https://humanbenchmark.com/tests/typing"
+    driver = webdriver.Firefox()
+    text = get_text(driver, url)
 
 
-driver = webdriver.Firefox()
-driver.get(url)
-source_code = driver.page_source
-soup = BeautifulSoup(source_code, "html.parser")
-spans = soup.find_all("span", class_="incomplete")
-typing_text = "".join([span.text for span in spans])
+
+
 pyautogui.write(typing_text, interval=0.035)
 
 print(divs)
